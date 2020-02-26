f51b45e6dea5e6680e95684cfeded7875ad6da16
MaksimKotau <maxkotov@gmail.com>
2020-02-25 22:31:46 -0500

    Add button

---
+++ src/components/AddButton.tsx
@@ -0,0 +1,28 @@
+import { createStyles, Fab, makeStyles, Theme } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import React from 'react';
+
+interface OwnProps {
+    onClick: () => void;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            position: "absolute",
+            right: 50,
+            bottom: 50
+        }
+    }),
+);
+
+const AddButton: React.FC<OwnProps> = (props) => {
+    const classes = useStyles();
+    return (
+        <Fab color="secondary" aria-label="add" className={classes.root} onClick={props.onClick}>
+            <AddIcon />
+        </Fab>
+    )
+}
+
+export default AddButton;
