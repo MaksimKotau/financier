f35b7810199fb6e786b2227117043fe5a7317008
MaksimKotau <maxkotov@gmail.com>
2020-03-08 09:05:03 -0400

    Correction for test

--- src/data/reducers/__tests__/accountReducers.test.ts
+++ src/data/reducers/__tests__/accountReducers.test.ts
@@ -9,7 +9,7 @@ import accountReducer from '../accountReducers';
 describe('accountReducers', () => {
     it('should return state with new account', () => {
         const currentAccountID: string = uuidv4();
-        const currentAccount: AccountDTO = { id: currentAccountID, name: "Name", type: AccountType.Cash, startBalance: 0 };
+        const currentAccount: AccountDTO = { id: currentAccountID, name: "Name", type: AccountType.Credit, startBalance: 0 };
         const currentAction: AddAccountAction = { type: constants.ADD_ACCOUNT, account: currentAccount };
         const expectedState = [currentAccount];
         expect(accountReducer(undefined, currentAction)).toEqual(expectedState);
@@ -17,9 +17,9 @@ describe('accountReducers', () => {
     it('should return state with modified account', () => {
         const date = "2020-08-16";
         const currentAccountID: string = uuidv4();
-        const currentAccount = { id: currentAccountID, name: "Name", type: AccountType.Cash, startBalance: 0, startDate: date };
+        const currentAccount = { id: currentAccountID, name: "Name", type: AccountType.Credit, startBalance: 0, startDate: date };
         const currentState = [currentAccount];
-        const modifiedAccount = { id: currentAccountID, name: "Name2", type: AccountType.Cash, startBalance: 0, startDate: date };
+        const modifiedAccount = { id: currentAccountID, name: "Name2", type: AccountType.Credit, startBalance: 0, startDate: date };
         const action: ModifyAccountAction = {type: constants.MODIFY_ACCOUNT, account: modifiedAccount};
         const expectedState = [modifiedAccount];
         expect(accountReducer(currentState, action)).toEqual(expectedState);
