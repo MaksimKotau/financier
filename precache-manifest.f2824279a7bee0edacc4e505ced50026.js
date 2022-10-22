967854315626e2a85e032bb9468ddf02c5d04ba4
Maksim Kotau <maxkotov@gmail.com>
2022-10-22 09:41:21 -0400

    Updates

---
+++ precache-manifest.f2824279a7bee0edacc4e505ced50026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279e5ec2a4a3d28a34dee2a1ccffa468",
+    "url": "/financier/index.html"
+  },
+  {
+    "revision": "ae515ba0c5916bca1cdc",
+    "url": "/financier/static/css/main.ed78cbb4.chunk.css"
+  },
+  {
+    "revision": "52d7588382689767ed70",
+    "url": "/financier/static/js/2.4a16813d.chunk.js"
+  },
+  {
+    "revision": "48eb53d9b3ff501566b6127605631846",
+    "url": "/financier/static/js/2.4a16813d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae515ba0c5916bca1cdc",
+    "url": "/financier/static/js/main.c9026f28.chunk.js"
+  },
+  {
+    "revision": "ac8db85ba66c2136ccb4",
+    "url": "/financier/static/js/runtime-main.19a0babf.js"
+  }
+]);
